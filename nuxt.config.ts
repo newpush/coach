bc811246a1cb65894c199fd74c41959d9582c058
Laszlo Racz <lracz@newpush.com>
2026-01-24 12:03:50 +0100

    chore(config): enable devtools and update prisma environment loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ const sentryRelease = `${pkg.name}@${pkg.version}+${commitHash}`
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
+  devtools: { enabled: true },
 
   app: {
     head: {
