49f6b11aad162e2add10842b668bf6190e55f179
Laszlo Racz <lracz@newpush.com>
2026-01-26 19:25:20 +0100

    style(ui): increase pricing description min-height for consistent header alignment

--- app/components/landing/PricingPlans.vue
+++ app/components/landing/PricingPlans.vue
@@ -91,7 +91,7 @@
             </span>
           </div>
 
-          <p class="mt-3 text-sm text-gray-500 dark:text-gray-400 min-h-[3rem]">
+          <p class="mt-3 text-sm text-gray-500 dark:text-gray-400 min-h-[4.5rem]">
             {{ plan.description }}
           </p>
         </template>
